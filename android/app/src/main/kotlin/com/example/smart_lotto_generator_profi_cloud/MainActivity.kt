eda6a43e103bf8122b9f8514476f4437742fa680
root <root@localhost.localdomain>
2025-10-20 21:57:40 +0000

    Fix Kotlin MainActivity syntax for FlutterActivity

--- android/app/src/main/kotlin/com/example/smart_lotto_generator_profi_cloud/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/smart_lotto_generator_profi_cloud/MainActivity.kt
@@ -2,6 +2,4 @@ package com.example.smart_lotto_generator_profi_cloud
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
-
+class MainActivity : FlutterActivity()
